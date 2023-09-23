95092497284416667f34f7f89cf799070e243b23
MiraculosMinotaur <tambeteelmere@gmail.com>
2023-09-23 22:23:46 +0300

    Changes for ETS2 Specifics

--- main_sketch_GearStickForMargus/main_sketch_GearStickForMargus.ino
+++ main_sketch_GearStickForMargus/main_sketch_GearStickForMargus.ino
@@ -19,44 +19,16 @@
 #define MAIN_STICK_SIZE 6
 
 enum Buttons{
-  NEUTRAL_0 = 0,
-  NORMAL_LOW_1,
-  NORMAL_LOW_2,
-  NORMAL_LOW_3,
-  NORMAL_LOW_4,
-  NORMAL_LOW_5,
-  NORMAL_LOW_6,
-  NORMAL_HIGH_7,
-  NORMAL_HIGH_8,
-  NORMAL_HIGH_9,
-  NORMAL_HIGH_10,
-  NORMAL_HIGH_11,
-  NORMAL_HIGH_12,
-  REVERSE_13,
-  REVERSE_14,
-  REVERSE_15,
-  REVERSE_16,
-  REVERSE_17,
-  REVERSE_18,
-  LOW_RANGE_19,
-  LOW_RANGE_20,
-  LOW_RANGE_21,
-  LOW_RANGE_22,
-  LOW_RANGE_23,
-  LOW_RANGE_24,
-  LOWER_IMPULSE_25,
-  HIGHER_IMPULSE_26
-};
-
-enum Modes{
-  NORMAL = 0,
+  NORMAL_1 = 0,
+  NORMAL_2,
+  NORMAL_3,
+  NORMAL_4,
+  NORMAL_5,
+  NORMAL_6,
   REVERSE,
-  LOW_RANGE_MODE
-};
-
-enum Ranges{
-  LOW_RANGE = 0,
-  HIGH_RANGE
+  HIGH_RANGE,
+  IMPULSE_1,
+  IMPULSE_2
 };
 
 
@@ -65,9 +37,9 @@ const uint8_t ModePins[] = {MODE_PIN_1, MODE_PIN_2};
 const uint8_t ImpulsePins[] = {LOWER_IMPULSE_PIN, HIGHER_IMPULSE_PIN};
 
 // history
-Ranges range = LOW;
 uint8_t prevImpulseState = 0;
-Buttons prevGear = NEUTRAL_0;
+uint8_t prevModeState = 0;
+uint8_t prevGearState = 0;
 
 Joystick_ Joystick;
 
@@ -79,7 +51,7 @@ void setup() {
 
   // Initialize Joystick Library
   Joystick.begin();
-  Joystick.pressButton(NEUTRAL_0);
+  Joystick.pressButton(NORMAL_1);
 }
 
 void loop() 
@@ -88,8 +60,6 @@ void loop()
   bool inGear = false;
   bool inMode = false;
   bool inImpulse = false;
-  Buttons gear = NEUTRAL_0;
-  Modes mode = NORMAL;
 
   // Impulse and Low/high Selection
   for(int8_t i = sizeof(ImpulsePins); i > 0; i--)
@@ -97,63 +67,54 @@ void loop()
     if(!digitalRead(ImpulsePins[i-1]))
     {
       inImpulse = true;
-      range = i-1;
       if(i != prevImpulseState)
       {
-        Joystick.pressButton(LOWER_IMPULSE_25 + range);
+        Joystick.pressButton(IMPULSE_1 + i-1);
         prevImpulseState = i;
       }
     }
   }
   if(!inImpulse)
   {
-    Joystick.releaseButton(LOWER_IMPULSE_25);
-    Joystick.releaseButton(HIGHER_IMPULSE_26);
+    Joystick.releaseButton(IMPULSE_1);
+    Joystick.releaseButton(IMPULSE_2);
   }
 
-  //SixWay and mode selection 
+  // mode Latch Selection
+  for(int8_t i = sizeof(ModePins); i > 0; i--)
+  {
+    if(!digitalRead(ModePins[i-1]))
+    {
+      inMode = true;
+      if(i != prevModeState)
+      {
+        Joystick.pressButton(REVERSE + i-1);
+        prevModeState = i;
+      }
+    }
+  }
+  if(!inMode)
+  {
+    Joystick.releaseButton(REVERSE);
+    Joystick.releaseButton(HIGH_RANGE);
+  }
+
+  //SixWay
   for(int8_t i = sizeof(SixWayPins); i > 0; i--) 
   {
     if(!digitalRead(SixWayPins[i-1]))
     {
       inGear = true;
-      gear = i;
+      if(i != prevGearState)
+      {
+        Joystick.pressButton(i-1);
+        prevGearState = i;
+      }
     }
   }
-  if(inGear)
+  if(!inGear)
   {
-      for(int8_t i = sizeof(ModePins); i > 0; i--)   
-      {
-        if(!digitalRead(ModePins[i-1]))
-        {
-          inMode = true;
-          mode = i;
-        }
-      }
-      switch(mode)
-      {
-        case NORMAL:
-          if(HIGH_RANGE == range){gear = gear + MAIN_STICK_SIZE;}
-          break;
-        case REVERSE:
-          gear = gear + MAIN_STICK_SIZE * 2;
-          break;
-        case LOW_RANGE_MODE:
-          gear = gear + MAIN_STICK_SIZE * 3;
-          break;
-        default:break;
-        //This should not happen
-      }
-
+    for(int8_t i = NORMAL_6;i > NORMAL_1-1;i--){Joystick.releaseButton(i);}
   }
-  else{gear = 0;}
-  if(gear != prevGear)
-  {
-    for(int8_t i = LOW_RANGE_24;i > NEUTRAL_0-1;i--){Joystick.releaseButton(i);}
-    Joystick.pressButton(gear);
-    prevGear = gear;
-  }
-
-
   delay(50);
 }
