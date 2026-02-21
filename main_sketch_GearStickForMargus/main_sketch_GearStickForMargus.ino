5fa1897e4ec4c4bfd908768c75eb3d415558cc4e
MiraculousMinotaur <tambeteelmere@gmail.com>
2026-02-21 14:13:01 +0200

    Serial cleanup and verification

--- main_sketch_GearStickForMargus/main_sketch_GearStickForMargus.ino
+++ main_sketch_GearStickForMargus/main_sketch_GearStickForMargus.ino
@@ -13,19 +13,14 @@
 #endif
 #include <Wire.h>
 
-#if DEBUG
-#define DEBUG_PRINT(...) Serial.print(__VA_ARGS__)
-#define DEBUG_PRINTLN(...) Serial.println(__VA_ARGS__)
-#else
-#define DEBUG_PRINT(...)
-#define DEBUG_PRINTLN(...)
-#endif
-
 // Joystick object is defined in JoystickGlue.cpp and declared extern in Config.h
 
 void setup() {
 #if DEBUG
   Serial.begin(115200);
+  uint32_t start = millis();
+  while (!Serial && millis() - start < 5000) { ; }
+  Serial.println("Serial started");
   DebugManager_init();
 #endif
 
